82556dc775ce1d2a62984415323d9ed1cd7d8a59
RubyCouz <cousin.ruby.cedric@gmail.com>
2021-09-10 11:35:55 +0200

    ajout vérification des données reçu lors de l'ajout d'un jeu

--- graphql/resolvers/games.js
+++ graphql/resolvers/games.js
@@ -1,4 +1,4 @@
-const Game = require ('../../models/game')
+const Game = require('../../models/game')
 const User = require('../../models/user')
 const {transformGame} = require('./merge')
 
@@ -24,7 +24,7 @@ module.exports = {
      * @returns {Promise<{[p: string]: *}>}
      */
     createGame: async (args, req) => {
-        if(!req.isAuth.valid) {
+        if (!req.isAuth.valid) {
             throw new Error('Vous devez être connecté pour effectuer cette action !!!')
         }
         // regex pour vérifier les données envoyées par l'appli
@@ -33,38 +33,41 @@ module.exports = {
         let validData = []
         const verifData = (regex, data) => {
             console.log(regexText)
-            if(regex.test(data)) {
+            if (regex.test(data)) {
                 data.trim()
                 validData.push(data)
+                return true
             } else {
                 throw new Error('Le champs ' + data + ' contient des caractères invalides')
             }
-            console.log(validData)
         }
         // vérification des données saisie
-        verifData(regexText, args.gameInput.game_name)
-        verifData(regexText, args.gameInput.game_desc)
-        verifData(regexText, args.gameInput.game_pic)
-        const game = new Game({
-            game_name: validData[0],
-            game_desc: validData[1],
-            game_pic: validData[2],
-            game_creator: req.isAuth.userId
-        })
-        let createdGame
-
-        try {
-            const result = await game.save()
-            createdGame = transformGame(result)
-            const game_creator = await User.findById(req.isAuth.userId)
-            if(!game_creator) {
-                throw new Error('Utilisateur inconnu !!!')
+        if (verifData(regexText, args.gameInput.game_name) && verifData(regexText, args.gameInput.game_desc) && verifData(regexText, args.gameInput.game_pic)) {
+            // instanciation d'un nouvel objet game
+            const game = new Game({
+                game_name: validData[0],
+                game_desc: validData[1],
+                game_pic: validData[2],
+                game_creator: req.isAuth.userId
+            })
+            let createdGame
+            try {
+                // envoie des données dans la base
+                const result = await game.save()
+                createdGame = transformGame(result)
+                // recherche du user qui fait la création
+                const game_creator = await User.findById(req.isAuth.userId)
+                // si l'utilisateur n'existe pas
+                if (!game_creator) {
+                    throw new Error('Utilisateur inconnu !!!')
+                }
+                // sauvegarde du user dans l'objet game qui vient d'être créé
+                game_creator.user_addedGames.push(game)
+                await game_creator.save()
+                return createdGame
+            } catch (err) {
+                throw err
             }
-            game_creator.user_addedGames.push(game)
-            await game_creator.save()
-            return createdGame
-        } catch (err) {
-            throw err
         }
     },
 }
