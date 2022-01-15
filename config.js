d0173341fc5cd275cde06bb039827217fe3a73d4
RubyCouz <cousin.ruby.cedric@gmail.com>
2022-01-15 13:11:19 +0100

    fix pipeline

---
+++ config.js
@@ -0,0 +1,9 @@
+exports.DB = 'Eterelz'
+exports.DB_USER = 'RubyCouz'
+exports.DB_PASSWORD = 'RubyCouz2805'
+exports.DB_SPACE = 'eterelz'
+
+exports.EMAIL = 'admin@rubycouz.xyz'
+exports.PASSWORD = '#Couz2805'
+
+exports.HOST = 'http://localhost:3000'
